267c7e86a2c5854848200d239d69b4c75188aaf8
Paul Plant <mail@paulplant.com>
2024-03-20 15:31:49 -0700

    implemented color as a class

--- include/color/color.hpp
+++ include/color/color.hpp
@@ -2,8 +2,16 @@
 #define COLOR_H
 
 #include "vec3.hpp"
+class color : public vec3 {
+    public:
+        color() : vec3(0, 0, 0) {}
+        color(double e0, double e1, double e2) : vec3(e0, e1, e2) {}
 
-using color = vec3;
+        color(const vec3 &other) : vec3(other) {}
+        double r() { return e[0]; }
+        double g() { return e[1]; }
+        double b() { return e[2]; }
+};
 
 const double COLOR_MAX = 255.999;
 
